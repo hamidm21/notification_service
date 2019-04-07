ca3463b4bcf61cca2312ee32c0143c6bf4bb8633
hamid <hordimad21@gmail.com>
2019-04-07 13:22:14 +0430

    __ADD__ SMS support in notification service

---
+++ routes/Sms.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../lib/controllers/sms_controller');
+
+router.post('/', function() {});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const Email = require('./Email');
+const Sms = require('./Sms');
 
 module.exports = (app) => {
     app.use('/Email', Email);
+    app.use('/Sms', Sms);
 }
